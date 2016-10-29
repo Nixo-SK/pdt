fc149a2b14b5132c245048e7c0edad180763f390
Tomáš Mikuška <mikuska.tom@gmail.com>
2016-10-29 23:50:45 +0200

    Connection to db allowed

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+import sys
+import getpass
+import psycopg2
 from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return send_from_directory('./', 'index.html')
+    return send_from_directory('./', 'index.html')		
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+	db = psycopg2.connect("host='localhost' dbname='postgres' user='postgres' password='{0}'".format(getpass.getpass()))
+	app.debug = True
+	app.run()
