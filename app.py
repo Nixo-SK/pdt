e148d3cc6c8eeb8c01c04d1250194c96d04c77c4
Tomáš Mikuška <mikuska.tom@gmail.com>
2016-10-30 16:28:16 +0100

    Simple query to show objects on map

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 import sys
+import json
 import getpass
 import psycopg2
-from flask import Flask, send_from_directory
+from flask import Flask, jsonify, request, send_from_directory
 
 app = Flask(__name__)
+db = psycopg2.connect("host='localhost' dbname='postgres' user='postgres' password='{0}'".format(getpass.getpass()))
+cursor = db.cursor()
+
+def parse_JSON(obj):
+	lng, lat = obj[0][16:-1].split()
+	return json.dumps({'lng': lng, 'lat': lat})
 
 @app.route('/')
 def index():
-    return send_from_directory('./', 'index.html')		
+	return send_from_directory('', 'index.html')		
 
+@app.route('/query')
+def query():	
+	try:
+		#lat = request.args.get('lat')
+		#lng = request.args.get('lng')
+		cursor.execute("select ST_AsEWKT(way) from planet_osm_point where amenity like 'hospital' or amenity like 'pharmacy'")
+		return jsonify([parse_JSON(res) for res in cursor])
+	except Exception as e:
+		return jsonify(e)	
+	
 if __name__ == '__main__':
-	db = psycopg2.connect("host='localhost' dbname='postgres' user='postgres' password='{0}'".format(getpass.getpass()))
 	app.debug = True
 	app.run()
