254b30e8baa28cd369409ed83c93e3cbc149ca63
Tomáš Mikuška <mikuska.tom@gmail.com>
2016-11-23 00:02:05 +0100

    Reduced overhead in first scenario

--- app.py
+++ app.py
@@ -8,16 +8,6 @@ app = Flask(__name__)
 db = psycopg2.connect("host='localhost' dbname='postgres' user='postgres' password='{0}'".format(getpass.getpass()))
 cursor = db.cursor()
 
-def parse_JSON(obj):
-	try:
-		lng, lat = obj[1][16:-1].split()
-		type = obj[0]
-	except IndexError:
-		lng, lat = obj[0][16:-1].split()
-		type = 'global'
-	finally:
-		return json.dumps({'type': type, 'lng': lng, 'lat': lat})
-
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory('static', 'favicon.ico')
@@ -26,17 +16,11 @@ def favicon():
 def index():
 	return send_from_directory('', 'index.html')		
 
-@app.route('/query')
-def query():	
-	try:
-		#lat = request.args.get('lat')
-		#lng = request.args.get('lng')
-		cursor.execute("select amenity, ST_AsEWKT(way) from planet_osm_point where amenity like 'hospital' or amenity like 'pharmacy'")
-		# cursor.execute("select ST_AsEWKT(way) from planet_osm_point where amenity like 'hospital' or amenity like 'pharmacy'")
-		return jsonify([parse_JSON(res) for res in cursor])
-	except Exception as e:
-		return jsonify(e)	
-	
+@app.route('/show_all')
+def show_all():	
+	cursor.execute("select amenity, ST_ASGEOJSON(way) from planet_osm_point where amenity in ('hospital', 'pharmacy')")
+	return jsonify([json.dumps({'type': row[0], 'lng': json.loads(row[1])['coordinates'][0], 'lat': json.loads(row[1])['coordinates'][1]}) for row in cursor])
+
 if __name__ == '__main__':
 	app.debug = True
 	app.run()
