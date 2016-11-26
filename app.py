3a152d538e176415493cebf2d19308e8591aacfc
Tomáš Mikuška <mikuska.tom@gmail.com>
2016-11-26 04:17:41 +0100

    Objects are now shown only in district by click (ST_WITHIN function)

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import json
 import getpass
 import psycopg2
-import coord_parser
 from flask import Flask, jsonify, request, send_from_directory
 from queries import *
 from settings import *
@@ -14,6 +13,13 @@ db = psycopg2.connect("host={} dbname={} user={} password={}".format(
 	DB_HOST, DB_NAME, DB_USER, DB_PASS if DEBUG else getpass.getpass()))
 cursor = db.cursor()
 
+def parse_result(rows):
+	return jsonify([
+		json.dumps({
+			'type': row[0], 
+			'lng': json.loads(row[1])['coordinates'][0], 
+			'lat': json.loads(row[1])['coordinates'][1]}) 
+		for row in rows])
 
 @app.route('/favicon.ico')
 def favicon():
@@ -27,34 +33,50 @@ def index():
 def available_types():
 	return jsonify({'types': AMENITY_TYPES});
 
-@app.route('/region_list')
+@app.route('/district_list')
 def region_list():
-	cursor.execute(REGION_LIST_QUERY)
-
-	region_dict = {}
-	for region in cursor.fetchall():
+	'''
+	* finds all districts by region and parse their coordinates
+	'''
+	cursor.execute(DISTRICT_LIST_QUERY)
+	district_dict = {}
+	for row in cursor.fetchall():
 		district = {
-			'name': region[1], 
-			'coords': json.loads(region[2][35:-1])
+			'name': row[1], 
+			'coords': json.loads(row[2][35:-1])
 		}
 
-		if region[0] in region_dict:
-			region_dict[region[0]].append(district)
+		if row[0] in district_dict:
+			district_dict[row[0]].append(district)
 		else:
-			region_dict[region[0]] = [district]
+			district_dict[row[0]] = [district]
 	
-	return jsonify([json.dumps({
-		'region': k, 'districts': v}) for k, v in sorted(region_dict.items())])
+	return jsonify([
+		json.dumps({
+			'region': k, 
+			'districts': v}) 
+		for k, v in sorted(district_dict.items())])
 
 @app.route('/show_all')
 def show_all():	
+	'''
+	Scenario 1
+	* shows all requested objects
+	* objects can be filtered by type (amenity)
+	'''
 	cursor.execute(SHOW_ALL_QUERY)
-	return jsonify([
-		json.dumps({
-			'type': row[0], 
-			'lng': json.loads(row[1])['coordinates'][0], 
-			'lat': json.loads(row[1])['coordinates'][1]}) 
-		for row in cursor.fetchall()])
+	return parse_result(cursor.fetchall())
+
+@app.route('/show_by_district')
+def show_by_district():	
+	'''
+	Scenario 2
+	* shows all requested objects that belong to distrinct parsed as a parameter
+	* objects still can be filtered by type (amenity)
+	'''
+	cursor.execute(SHOW_BY_DISTRICT_QUERY.replace(
+		'district', '\'' + request.args.get('district') + '\''))
+	return parse_result(cursor.fetchall())
 
 if __name__ == '__main__':
 	app.run()
--- queries.py
+++ queries.py
@@ -1,6 +1,6 @@
 from settings import AMENITY_TYPES
 
-REGION_LIST_QUERY = """
+DISTRICT_LIST_QUERY = """
 	SELECT region.name, district.name, ST_ASGEOJSON(district.way)
 	FROM planet_osm_line AS region 
 	JOIN planet_osm_line AS district 
@@ -10,12 +10,18 @@ REGION_LIST_QUERY = """
 	ORDER BY region.name, district.name
 """
 
-DISTRICT_COORD_QUERY = """
-	SELECT ST_ASGEOJSON(way)
-"""
-
 SHOW_ALL_QUERY = """
 	SELECT amenity, ST_ASGEOJSON(way)
 	FROM planet_osm_point 
 	WHERE amenity IN ('{}')
+""".format('\',\''.join(AMENITY_TYPES))
+
+SHOW_BY_DISTRICT_QUERY = """
+	SELECT amenity, ST_ASGEOJSON(way)
+	FROM planet_osm_point
+	WHERE amenity IN ('{}')
+	AND ST_WITHIN(way,
+	    (SELECT way
+		FROM planet_osm_polygon
+		WHERE name = district))
 """.format('\',\''.join(AMENITY_TYPES))
